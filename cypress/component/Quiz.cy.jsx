b666dc30d1ded06ac7e635b4963a5ac7d68ef3fb
lwebert <lauren@weberts.org>
2025-04-17 21:15:30 -0500

    Working on some e2e changes

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,19 +1,73 @@
-import React from 'react';
-import Quiz from '../../client/src/components/Quiz';
+// import React from 'react';
+import Quiz from '../../client/src/components/Quiz.jsx';
+
+// const questions = [
+// 	// const questions = [
+// 	{
+// 		_id: '1',
+// 		question: 'What is the capital of France?',
+// 		answers: [
+// 			{ text: 'Paris', isCorrect: true },
+// 			{ text: 'London', isCorrect: false },
+// 			{ text: 'Berlin', isCorrect: false },
+// 			{ text: 'Rome', isCorrect: false },
+// 		],
+// 	},
+// 	{
+// 		_id: '2',
+// 		question: 'Which planet is known as the Red Planet?',
+// 		answers: [
+// 			{ text: 'Earth', isCorrect: false },
+// 			{ text: 'Mars', isCorrect: true },
+// 			{ text: 'Jupiter', isCorrect: false },
+// 			{ text: 'Saturn', isCorrect: false },
+// 		],
+// 	},
+// 	{
+// 		_id: '3',
+// 		question: 'Who wrote "Romeo and Juliet"?',
+// 		answers: [
+// 			{ text: 'William Shakespeare', isCorrect: true },
+// 			{ text: 'Mark Twain', isCorrect: false },
+// 			{ text: 'Jane Austen', isCorrect: false },
+// 			{ text: 'Charles Dickens', isCorrect: false },
+// 		],
+// 	},
+// 	{
+// 		_id: '4',
+// 		question: 'Which gas do humans need to breathe?',
+// 		answers: [
+// 			{ text: 'Carbon Dioxide', isCorrect: false },
+// 			{ text: 'Oxygen', isCorrect: true },
+// 			{ text: 'Hydrogen', isCorrect: false },
+// 			{ text: 'Nitrogen', isCorrect: false },
+// 		],
+// 	},
+// 	{
+// 		_id: '5',
+// 		question: 'What is the color of the sky?',
+// 		answers: [
+// 			{ text: 'Blue', isCorrect: true },
+// 			{ text: 'Green', isCorrect: false },
+// 			{ text: 'Red', isCorrect: false },
+// 			{ text: 'Orange', isCorrect: false },
+// 		],
+// 	},
+// ];
 
 describe('<Quiz />', () => {
-    it('Should render the Quiz component.', () => {
-        cy.mount(<Quiz />)
-    });
+	it('Should render the Quiz component.', () => {
+		cy.mount(<Quiz />);
+	});
 
-    it('Should render the Quiz component before the quiz is started, and display a button.', () => {
-        cy.mount(<Quiz />)
-        cy.get('button').should('.have.text', 'Start Quiz')
-    })
+	it('Should render the Quiz component before the quiz is started, and display a button.', () => {
+		cy.mount(<Quiz />);
+		cy.get('button').should('have.text', 'Start Quiz');
+	});
 
-    it('Should render the Quiz component after the quiz has started, and display the current question.', () => {
-        cy.mount(<Quiz />)
-        cy.get('h2').should('.have.text')
-
-    })
-})
+	it('Should render the Quiz component after the quiz has started, and display the current question.', () => {
+		cy.mount(<Quiz />);
+		cy.get('button').click(); // TODO: Start the quiz ??
+		cy.get('h2').should('exist');
+	});
+});
--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,23 +1,49 @@
-import { mockState } from '../support/utils/helpers';
+// import { mockQuestions } from '../support/utils/helpers';
+// import { Responses } from '../support/types'
 
-describe('Tech Quiz Test Suite', () => {
+describe('Tech Quiz End-to-End Test Suite', () => {
     context('Quiz Setup', () => {
         beforeEach(() => {
+            cy.visit('http://localhost:3000');
             cy.intercept('GET', 'api/questions/random', {
-                body: mockState,
+                // body: mockQuestions,
                 statusCode: 200,
+                fixture: 'questions.json'
+            }).as('fixtureQuestions');
+
+        });
+
+        it('Should render the "Start Quiz" button on page load.', () => {
+            cy.findByText('Start Quiz').should('be.visible');
+        })
+
+        it('Upon quiz start, should render a random question with answer options.', () => {
+            cy.get('button').contains('Start Quiz').click();
+        })
+
+        it('Unit test: Should GET a random question on page load, and render the random question to the page.', () => {
+            cy.contains('Start Quiz').click();
+
+            cy.wait('@fixtureQuestions').its('response.statusCode').should('eq', 200);
+
+            cy.get('h2').should('exist').and('not.be.empty');
+        });
+    });
+
+    context('User Taking Quiz', () => {
+        beforeEach(() => {
+            cy.intercept('GET', '/api/game/start', {
+                statusCode: 200,
+                body: mockQuestions,
             }).as('getRandomQuestion');
 
             cy.visit('/');
+            cy.contains('Start Quiz').click();
+            cy.wait('@getRandomQuestion');
         });
 
-        it('Unit test: Should GET a random question on page load, and render the random question to the page.')
-
-
-
+        it('Should allow the user to answer each question.')
     });
-
-    context('User Taking Quiz', () => { });
 
     context('Quiz Restart', () => { });
 })
